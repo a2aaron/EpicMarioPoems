e24bcb106ec41897efbae0e9a5cb871b92989e1e
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-10-29 13:48:56 -0400

    Move access token stuff into its own function for simplicity.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,17 @@ use tokio_core::reactor::Core;
 use name_gen::random_msg;
 
 fn main() {
+    let token = get_token(); 
+
+    let epic = random_msg();
+    let tweet = DraftTweet::new(&epic);
+    
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    core.run(tweet.send(&token, &handle)).unwrap();
+}
+
+fn get_token() -> egg_mode::Token {
     dotenv().ok();
 
     let consumer_key = env::var("CONSUMER_KEY").unwrap();
@@ -25,15 +36,9 @@ fn main() {
 
     let con_token = egg_mode::KeyPair::new(consumer_key, consumer_secret);
     let access_token = egg_mode::KeyPair::new(access_token, access_token_secret);
-    let token = egg_mode::Token::Access {
+
+    egg_mode::Token::Access {
         consumer: con_token,
         access: access_token,
-    };
-
-    let epic = random_msg();
-    let tweet = DraftTweet::new(&epic);
-    
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    core.run(tweet.send(&token, &handle)).unwrap();
+    }
 }
