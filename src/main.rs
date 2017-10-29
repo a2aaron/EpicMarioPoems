b98786fb67dc9cb9e8a615d4f8815f55d992ee16
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-10-29 01:13:01 -0400

    Add dotenv and read consumer/access keys from .env file.
    .env is placed into the .gitignore to avoid accidentially commiting it.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 extern crate egg_mode;
+extern crate dotenv;
 
 mod name_gen;
 mod epic;
 
+use dotenv::dotenv;
+use std::env;
+use name_gen::*;
+
+
+
 fn main() {
+    dotenv().ok();
+
+    let consumer_key = env::var("CONSUMER_KEY");
+    let consumer_secret = env::var("CONSUMER_SECRET");
+    let access_token = env::var("ACCESS_TOKEN");
+    let access_token_secret = env::var("ACCESS_TOKEN_SECRET");
+
     for epic in epic::EPICS {
         match epic.wiki() {
             Some(wiki) => {
