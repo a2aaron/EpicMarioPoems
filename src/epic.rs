2fd7ce532a166b29baa9987d2ca247e9051d9b91
Caleb Jones <code@calebjones.net>
2017-10-28 23:00:25 -0400

    Add a method to get the Wikipedia URL for a poem

--- src/epic.rs
+++ src/epic.rs
@@ -7,7 +7,29 @@ pub struct Epic {
     /// We store the Wikipedia article that we want to use for attribution. If a
     /// given poem doesn't have an appropriate Wikipedia article to link to,
     /// this will contain `None`.
-    pub wiki: Option<&'static str>,
+    wiki: Wiki,
+}
+
+impl Epic {
+    /// Returns the URL of the Wikipedia article that we want to use for
+    /// attribution. If a given poem doesn't have an appropriate Wikipedia
+    /// article to link to, this will be `None`.
+    pub fn wiki(&self) -> Option<String> {
+        let name = match self.wiki {
+            Wiki::None => return None,
+            Wiki::Same => self.title,
+            Wiki::Name(name) => name,
+        };
+        let name = name.replace(" ", "_");
+        Some(format!("https://en.wikipedia.org/wiki/{}", name))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Wiki {
+    None,
+    Same,
+    Name(&'static str),
 }
 
 /// A list of epic poems
@@ -15,47 +37,97 @@ pub static EPICS: &[Epic] = &[
     // 8th to 6th century BC
     Epic {
         title: "Epic of Gilgamesh",
-        wiki: Some("Epic of Gilgamesh"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Atrahasis",
-        wiki: Some("Atra-Hasis"),
+        wiki: Wiki::Name("Atra-Hasis"),
     },
     Epic {
-        title: "Enuma Elish ",
-        wiki: Some("Enuma Elish"),
+        title: "Enûma Eliš",
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Legend of Keret",
-        wiki: Some("Legend of Keret"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Cycle of Kumarbi",
-        wiki: Some("Kumarbi"),
+        wiki: Wiki::Same,
     },
     // 20th to 10th century BC
     Epic {
         title: "Iliad",
-        wiki: Some("Iliad"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Odyssey",
-        wiki: Some("Odyssey"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Works and Days",
-        wiki: Some("Works and Days"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Theogony",
-        wiki: Some("Theogony"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Shield of Heracles",
-        wiki: Some("Shield of Heracles"),
+        wiki: Wiki::Same,
     },
     Epic {
         title: "Catalogue of Women",
-        wiki: Some("Catalogue of Women"),
+        wiki: Wiki::Same,
     },
 ];
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_wiki_url_same() {
+        assert_eq!(
+            Epic {
+                title: "Odyssey",
+                wiki: Wiki::Same,
+            }.wiki(),
+            Some("https://en.wikipedia.org/wiki/Odyssey".into())
+        );
+    }
+
+    #[test]
+    fn test_wiki_url_different() {
+        assert_eq!(
+            Epic {
+                title: "Atrahasis",
+                wiki: Wiki::Name("Atra-Hasis"),
+            }.wiki(),
+            Some("https://en.wikipedia.org/wiki/Atra-Hasis".into())
+        );
+    }
+
+    #[test]
+    fn test_wiki_url_none() {
+        assert_eq!(
+            Epic {
+                title: "Dona Branca",
+                wiki: Wiki::None,
+            }.wiki(),
+            None
+        );
+    }
+
+    #[test]
+    fn test_wiki_url_spaces() {
+        assert_eq!(
+            Epic {
+                title: "Epic of Gilgamesh",
+                wiki: Wiki::Same,
+            }.wiki(),
+            Some("https://en.wikipedia.org/wiki/Epic_of_Gilgamesh".into())
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod epic;
 
 fn main() {
     for epic in epic::EPICS {
-        match epic.wiki {
+        match epic.wiki() {
             Some(wiki) => {
                 let title = name_gen::super_mario_epic(epic.title);
                 println!("{} ({})", title, wiki);
