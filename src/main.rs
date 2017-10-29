5f36b62925b8007f3b43f7be9364b6bdce50acf2
Caleb Jones <code@calebjones.net>
2017-10-29 00:15:15 -0400

    Select a random epic to use as a message
    Close #7

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
+extern crate rand;
+
 mod name_gen;
 mod epic;
 
+
 fn main() {
-    for epic in epic::EPICS {
-        match epic.wiki() {
-            Some(wiki) => {
-                let title = name_gen::super_mario_epic(epic.title);
-                println!("{} ({})", title, wiki);
-            }
-            None => {
-                println!(
-                    "{}",
-                    name_gen::super_mario_epic(epic.title),
-                )
-            }
-        }
-    }
+    println!("{}", name_gen::random_msg());
 }
--- src/name_gen.rs
+++ src/name_gen.rs
@@ -1,5 +1,33 @@
+use rand::{self, Rng};
+use epic;
+
+
+pub fn random_msg() -> String {
+    let epic = select_epic();
+    epic_msg(epic)
+}
+
 pub fn super_mario_epic(epic_title: &str) -> String {
     format!("Super Mario {}", epic_title)
+}
+
+fn select_epic() -> &'static epic::Epic {
+    rand::thread_rng().choose(epic::EPICS).unwrap()
+}
+
+fn epic_msg(epic: &epic::Epic) -> String {
+    match epic.wiki() {
+        Some(wiki) => {
+            let title = super_mario_epic(epic.title);
+            format!("{}\n\n({})", title, wiki)
+        }
+        None => {
+            format!(
+                "{}",
+                super_mario_epic(epic.title),
+            )
+        }
+    }
 }
 
 #[cfg(test)]
@@ -9,7 +37,10 @@ mod tests {
     #[test]
     fn test_super_mario_epic() {
         assert_eq!(super_mario_epic("Illiad"), "Super Mario Illiad");
-        assert_eq!(super_mario_epic("Epic of Gilgamesh"), "Super Mario Epic of Gilgamesh");
+        assert_eq!(
+            super_mario_epic("Epic of Gilgamesh"),
+            "Super Mario Epic of Gilgamesh"
+        );
         assert_eq!(super_mario_epic("Odyssey"), "Super Mario Odyssey");
     }
 }
