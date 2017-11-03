612078c0748227ee9359d27c40583be08644a21f
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-11-02 21:18:05 -0400

    Make TokenError an Enum and simplify the get_token error handling logic.
    Making TokenError an enum makes it way easier to early return if we can't read out of .env for some reason.

--- src/main.rs
+++ src/main.rs
@@ -38,23 +38,17 @@ fn main() {
 fn get_token() -> Result<egg_mode::Token, TokenError> {
     dotenv().ok();
 
-    let consumer_key = env::var("CONSUMER_KEY");
-    let consumer_secret = env::var("CONSUMER_SECRET");
-    let access_token = env::var("ACCESS_TOKEN");
-    let access_token_secret = env::var("ACCESS_TOKEN_SECRET");
+    let consumer_key = env::var("CONSUMER_KEY")
+        .map_err(TokenError::ConsumerKey)?;
+    let consumer_secret = env::var("CONSUMER_SECRET")
+        .map_err(TokenError::ConsumerSecret)?;
+    let access_token = env::var("ACCESS_TOKEN")
+        .map_err(TokenError::AccessToken)?;
+    let access_token_secret = env::var("ACCESS_TOKEN_SECRET")
+        .map_err(TokenError::AccessTokenSecret)?;
 
-    if consumer_key.is_err() || consumer_secret.is_err() ||
-       access_token.is_err() || access_token_secret.is_err() {
-        return Err(TokenError {
-            consumer_key: consumer_key.err(),
-            consumer_secret: consumer_secret.err(),
-            access_token: access_token.err(),
-            access_token_secret: access_token_secret.err(),
-        })
-    }
-
-    let con_token = egg_mode::KeyPair::new(consumer_key.unwrap(), consumer_secret.unwrap());
-    let access_token = egg_mode::KeyPair::new(access_token.unwrap(), access_token_secret.unwrap());
+    let con_token = egg_mode::KeyPair::new(consumer_key, consumer_secret);
+    let access_token = egg_mode::KeyPair::new(access_token, access_token_secret);
 
     Ok(egg_mode::Token::Access {
         consumer: con_token,
@@ -62,10 +56,11 @@ fn get_token() -> Result<egg_mode::Token, TokenError> {
     })
 }
 
+use std::env::VarError;
 #[derive(Debug)]
-struct TokenError {
-    consumer_key: Option<std::env::VarError>,
-    consumer_secret: Option<std::env::VarError>,
-    access_token: Option<std::env::VarError>,
-    access_token_secret: Option<std::env::VarError>,
+enum TokenError {
+    ConsumerKey(VarError),
+    ConsumerSecret(VarError),
+    AccessToken(VarError),
+    AccessTokenSecret(VarError),
 }
